7314064eb691cb7d3d6193cce6b8fed5094cb829
Travis Struck <tjstruck@vm64-30.iplantcollaborative.org>
2016-06-11 18:27:32 -0700

    Add subplot B

--- capstone_script.py
+++ capstone_script.py
@@ -2,3 +2,11 @@
 Authors: Struck and Tha
 This script makes a figure with two subplots
 '''
+import pandas as pd
+
+dats = pd.read_csv('DATA/gapminder-FiveYearData.csv')
+
+afg_dats = dats[dats['country']=='Afghanistan']
+
+#plot subfplot B
+#bla
